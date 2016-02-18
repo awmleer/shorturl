7614f3cc04ca43251392c5e4e1e9c79bdc2d8bfa
haoguangbo <haoguangbo@yeah.net>
2016-02-18 12:07:45 +0800

    随机生成字符串函数

--- node/app.js
+++ node/app.js
@@ -16,6 +16,19 @@ connection.connect(function (err) {
 });
 
 
+/*随机产生字符串函数*/
+function randomstr(len){
+    var str = "",
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for(var i=0; i<len; i++){
+        pos = Math.round(Math.random() * (arr.length-1));
+        str += arr[pos];
+    }
+    return str;
+}
+
+
+
 /*test*/
 app.get('/test', function (req, res) {
     console.log("test success");
