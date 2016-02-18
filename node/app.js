a15cc6b71f6887872b2500af85477bb4ad491acb
haoguangbo <haoguangbo@yeah.net>
2016-02-18 09:46:41 +0800

    jump.html

--- node/app.js
+++ node/app.js
@@ -1,47 +1,69 @@
 var express = require('express');
+var mysql      = require('mysql');
 var app = express();
+var connection = mysql.createConnection({
+    host     : '121.42.209.162',
+    user     : 'root',//TODO 貌似这里不能是root啊。。
+    password : '86.corrode',
+    database : 'shorturl'
+});
 
+app.get('/test', function (req, res) {
+    console.log("test success");
+});
 
 app.get('/shorten', function (req, res) {
-    var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-        host     : '121.42.209.162',
-        user     : 'root',
-        password : '86.corrode',
-        database : 'shorturl'
-    });
-
-    connection.connect(function(err) {
+    connection.connect(function (err) {
         if (err) {
             console.error('error connecting: ' + err.stack);
             return;
         }
-        console.log('connected as id ' + connection.threadId);
+        console.log('connected  id:' + connection.threadId);
     });
 
-    var long=req.query.long;
+    var long = req.query.long;
     console.log(long);
-
-
-    connection.query("SELECT * FROM url WHERE long=\"test\"", function(err, rows) {
-        if(err){
+    connection.query("SELECT * FROM url WHERE long='" + long + "'", function (err, rows) {
+        if (err) {
             console.log(err.code); // 'ECONNREFUSED'
             console.log(err.fatal); // true
-            connection.query("INSERT INTO url (long,short) values('test1','test2')",function(err,rows){
-                if(err){
-                    console.log("insert error");
-                }else{
-                    res.send("inserted!");
-                }
-            });
-        }else{
+            res.send("fail");
+            //connection.query("INSERT INTO url (long,short) values('test1','test2')",function(err,rows){
+            //    if(err){
+            //        console.log("insert error");
+            //    }else{
+            //        res.send("inserted!");
+            //    }
+            //});
+        } else {
             res.send("ok");
         }
 
     });
+    connection.end();
+});
+
+app.get('find', function (req, res) {
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected  id:' + connection.threadId);
+    });
+
+    var short = req.query.short;
+    connection.query("SELECT * FROM url WHERE short='"+short+"'", function (err, rows) {
+        if (err) {
+            console.log(err.code);
+            console.log(err.fatal);
+            res.send("fail");
+        } else {
+            res.send(rows.long);
+        }
+    });
 
     connection.end();
-
 });
 
 var server = app.listen(3000, function () {
