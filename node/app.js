05fc4e1c630970b0ad12d6d40507b78887f1b3f0
haoguangbo <haoguangbo@yeah.net>
2016-02-18 12:53:10 +0800

    改了一些结构

--- node/app.js
+++ node/app.js
@@ -18,13 +18,13 @@ connection.connect(function (err) {
 
 /*随机产生字符串函数*/
 function randomstr(len){
-    var str = "",
+    var string = "",
         arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     for(var i=0; i<len; i++){
         pos = Math.round(Math.random() * (arr.length-1));
-        str += arr[pos];
+        string += arr[pos];
     }
-    return str;
+    return string;
 }
 
 
@@ -44,35 +44,81 @@ app.get('/shorten', function (req, res) {
     var longurl = req.query.longurl;
     var shorturl=req.query.shorturl;
     console.log("longurl:"+longurl+"\nshorturl:"+shorturl);
-    if (shorturl == "") {
 
-    }else {
-        connection.query("SELECT * FROM url WHERE longurl='" + longurl + "'", function (err, rows) {
+    function inserturl(){
+        //插入数据
+        var myDate=new Date();
+        var datetime=myDate.toLocaleString( );
+        connection.query("INSERT INTO url (longurl,shorturl,datetime) VALUES ('" + longurl + "','" + shorturl + "','"+datetime+"')", function (err, rows) {
             if (err) {
                 console.log(err.code);
                 console.log(err.fatal);
                 res.send("fail");
                 return;
             }
-            console.log("query1 success");
+            var data={
+                'longurl':longurl,
+                'shorturl':shorturl,
+                'datetime':datetime
+            };
+            res.send(data);
+        });
+    }
 
-            if (rows.length==0) {
-                var myDate=new Date();
-                var datetime=myDate.toLocaleString( );
-                connection.query("INSERT INTO url (longurl,shorturl,datetime) VALUES ('" + longurl + "','" + shorturl + "','"+datetime+"')", function (err, rows) {
+
+    //查询该longurl是否已经存在
+    connection.query("SELECT * FROM url WHERE longurl='" + longurl + "'", function (err, rows) {
+        if (err) {
+            console.log(err.code);
+            console.log(err.fatal);
+            res.send("fail");
+            return;
+        }
+        //如果存在则直接返回对应的shorturl，并且更新时间戳
+        console.log("query1 success");
+        if (rows.length != 0) {
+            res.send(rows[0].shorturl);
+            //todo 更新时间戳
+            return;
+        }
+
+        if (shorturl == "") {
+            //如果用户传入的shorturl为空则随机生成一个
+            var string;
+            var repeat=true;
+            var fail=false;
+            function generate() {
+                string = randomstr(1);
+                //console.log(string);
+                connection.query("SELECT * FROM url WHERE shorturl='" + string + "'", function (err, rows) {
                     if (err) {
                         console.log(err.code);
                         console.log(err.fatal);
                         res.send("fail");
+                        fail = true;
+                        repeat = false;
                         return;
                     }
-                    res.send("inserted!");
+                    console.log(rows.length);
+                    if (rows.length == 0) {
+                        shorturl=string;
+                        inserturl();
+                    } else {
+                        generate();
+                    }
                 });
-            } else {
-                res.send(rows[0].shorturl);
             }
-        });
-    }
+            generate();
+        }else{
+            //如果用户传入的shorturl不为空则检查是否重复
+
+        }
+
+
+
+    });
+
+
 
 
     //connection.end();
